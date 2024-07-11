f97b6965aec322add84446cda647382d4772e816
Sumanth Banka <bankasumanth@gmail.com>
2024-07-11 07:51:11 +0530

    main

--- phase1/patterns/pattern22.java
+++ phase1/patterns/pattern22.java
@@ -1,16 +1,18 @@
+import java.lang.Math;
 import java.util.Scanner;
-public class pattern21 {
+
+public class pattern22 {
     public static void pattern(int N) {
-        for(int i = 0 ; i<N ; i++){
-            for(int j = 0 ; j<N ; j++){
-                if(i==0 || i==N-1 || j==0 || j==N-1){
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
-                }
+        for(int i = 0 ; i<2*N-1 ; i++){
+            for(int j = 0 ; j<2*N-1 ; j++){
+                int top = i;
+                int bottom = 2*N-2-i;
+                int left = j;
+                int right = 2*N-2-j;
+                System.out.print(N-Math.min(Math.min(top, bottom), Math.min(left, right))); 
             }
-        }
-        System.out.println();
+            System.out.println();
+        }   
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,7 +23,6 @@ public class pattern21 {
             int n = sc.nextInt(); // Read N for each test case
             pattern(n); // Print the star pattern for the current test case
         }
-        sc.close(); // Close the scanner to prevent resource leak
+        sc.close();
     }
-    
 }
